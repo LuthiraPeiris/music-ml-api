73498b16de2c8ba5265f24d4a21677951696e4f4
Luthira Peiris <22cse0369@ms.sab.ac.lk>
2026-02-24 21:18:08 +0530

    Enable CORS

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import os
 
 app = Flask(__name__)
+CORS(app) 
 
 # Load model once at startup
 model = joblib.load("music_recommender.joblib")
